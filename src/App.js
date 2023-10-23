be924eec8a954335782b190e73c80f31d4f511d3
DESKTOP-1NFVDUO\ishift <j10345678@gmail.com>
2023-10-23 10:34:24 +0900

    useMemo를 사용해서 함수 최적화하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import DiaryEditor from "./DiaryEditor";
 import "./App.css";
 import DiaryList from "./DiaryList";
-import { useState, useRef, useEffect } from "react";
-import LifeCycle from "./Lifecycle";
-
-// https://jsonplaceholder.typicode.com/comments
+import { useState, useRef, useEffect, useMemo } from "react";
+import OptimizeTest from "./OptimizeTest";
 
 function App() {
   const [data, setData] = useState([]);
@@ -49,7 +47,6 @@ function App() {
 
   // 일기 삭제하는 함수
   const onRemove = (targetId) => {
-    console.log(`${targetId} 삭제`);
     // 해당 id 포함하지 않는 새로운 배열 생성
     const newDiaryList = data.filter((it) => it.id !== targetId);
     setData(newDiaryList);
@@ -62,9 +59,25 @@ function App() {
     );
   };
 
+  // useMemo로 부터 값을 리턴받는다.
+  const getDiaryAnalysis = useMemo(() => {
+    const goodCount = data.filter((it) => it.emotion >= 3).length;
+    const badCount = data.length - goodCount;
+    const goodRatio = (goodCount / data.length) * 100;
+    return { goodCount, badCount, goodRatio };
+  }, [data.length]); // [] 안에 있는 값이 바뀔 때만 재실행
+
+  const { goodCount, badCount, goodRatio } = getDiaryAnalysis;
+
   return (
     <div className="App">
+      <OptimizeTest />
+
       <DiaryEditor onCreate={onCreate} />
+      <div>전체 일기 : {data.length}</div>
+      <div>기분 좋은 일기 개수 : {goodCount}</div>
+      <div>기분 나쁜 일기 개수 : {badCount}</div>
+      <div>기분 좋은 일기 비율 : {goodRatio}% </div>
       <DiaryList diaryList={data} onRemove={onRemove} onEdit={onEdit} />
     </div>
   );
